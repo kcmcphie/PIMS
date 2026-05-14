527634fdf6eebbde972154664f7f77a9815ec0e1
Kenzie McPhie <kmcphie@uwyo.edu>
2026-05-14 17:27:38 -0500

    basemap update

--- map.js
+++ map.js
@@ -3,21 +3,128 @@ document.addEventListener("DOMContentLoaded", () => {
   require([
     "esri/Map",
     "esri/views/MapView",
-    "esri/widgets/ScaleBar"
-  ], (Map, MapView, ScaleBar) => {
+    "esri/widgets/ScaleBar",
+    "esri/layers/TileLayer",
+    "esri/layers/GeoJSONLayer",
+    "esri/layers/VectorTileLayer"
+  ], (
+    Map,
+    MapView,
+    ScaleBar,
+    TileLayer,
+    GeoJSONLayer,
+    VectorTileLayer
+  ) => {
+
+    const labelsLayer = new VectorTileLayer({
+      portalItem: {
+        id: "1768e8369a214dfab4e2167d5c5f2454"
+      },
+    });
 
     const map = new Map({
-      basemap: "topo-vector"
+      basemap: {
+        baseLayers: [],
+        referenceLayers: [labelsLayer]
+      }
     });
+
+    const hillshadeLayer = new TileLayer({
+      url: "https://services.arcgisonline.com/ArcGIS/rest/services/Elevation/World_Hillshade/MapServer"
+
+    });
+
+    const countriesLayer = new GeoJSONLayer({
+      url: "./world.geojson",
+
+      renderer: {
+        type: "simple",
+
+        symbol: {
+          type: "simple-fill",
+
+          color: [194, 194, 194, 0.35],
+
+          outline: {
+            color: [194, 194, 194],
+            width: 0.75
+          }
+
+        }
+    }
+
+    })
+
+    const statesLayer = new GeoJSONLayer({
+      url: "./states.geojson",
+
+      renderer: {
+        type: "simple",
+
+        symbol: {
+          type: "simple-fill",
+
+          color: [61, 109, 63, 0.35],
+
+          outline: {
+            color: [73, 73, 73],
+            width: 0.75
+          }
+        }
+      }
+    });
+
+    const riversLayer = new GeoJSONLayer({
+      url: "./rivers.geojson",
+
+      renderer: {
+        type: "simple",
+
+        symbol: {
+          type: "simple-line",
+
+          color: [0, 98, 163, 0.3],
+
+          width: 1.2
+        }
+      }
+    });
+
+    const NAriversLayer = new GeoJSONLayer({
+      url: "./NArivers.geojson",
+
+      renderer: {
+        type: "simple",
+
+        symbol: {
+          type: "simple-line",
+
+          color: [0, 98, 163, 0.3],
+
+          width: 1.2
+        }
+      }
+    });
+
+    map.add(hillshadeLayer);
+    map.add(countriesLayer);
+    map.add(statesLayer);
+    map.add(riversLayer);
+    map.add(NAriversLayer);
 
     window.view = new MapView({
       container: "map",
       map: map,
+
       center: [-107.5512, 42.9993],
       zoom: 7,
 
+      spatialReference: {
+        wkid: 3857
+      },
+
       constraints: {
-          rotationEnabled: false
+        rotationEnabled: false
       }
     });
 
@@ -31,15 +138,15 @@ document.addEventListener("DOMContentLoaded", () => {
       position: "bottom-left"
     });
 
-    view.ui.move("attribution", "top-right");
+    window.view.ui.move("attribution", "top-right");
 
     window.mapReady = false;
 
     window.view.when(async () => {
 
-        window.mapReady = true;
+      window.mapReady = true;
 
-        console.log("Map fully ready");
+      console.log("Map fully ready");
 
     });
 
