3b1c749f741e6b059dbf336c6c293da42d0b4be3
Kenzie McPhie <kmcphie@uwyo.edu>
2026-06-22 16:56:13 -0500

    legend update

--- js/widgets/legend.js
+++ js/widgets/legend.js
@@ -24,6 +24,48 @@ define([
         {
           layer: view.map.findLayerById("contours"),
           title: "10m Contours"
+        },
+
+        // SBM Blocks 
+        {
+          layer: view.map.findLayerById("emmablock_poly"),
+          title: "Emma Block"
+        },
+        {
+          layer: view.map.findLayerById("goldsmithblock_poly"),
+          title: "Goldsmith Block"
+        },
+        {
+          layer: view.map.findLayerById("greatrep_poly"),
+          title: "Great Republic Block"
+        },
+        {
+          layer: view.map.findLayerById("margetann_poly"),
+          title: "Marget Ann Block"
+        },
+        {
+          layer: view.map.findLayerById("rainbow_poly"),
+          title: "Rainbow Block"
+        },
+        {
+          layer: view.map.findLayerById("travona_poly"),
+          title: "Travona Block"
+        },
+        {
+          layer: view.map.findLayerById("ferrylane_poly"),
+          title: "Ferry Lane"
+        },
+        {
+          layer: view.map.findLayerById("claims_greatrep_poly"),
+          title: "Great Republic Claims"
+        },
+        {
+          layer: view.map.findLayerById("claims_margetann_poly"),
+          title: "Marget Ann Claims"
+        },
+        {
+          layer: view.map.findLayerById("claims_rainbow_poly"),
+          title: "Rainbow Claims"
         }
       ]
     });
